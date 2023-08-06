a9689d73387dfaf694ff0a54c7f5b0815fd97407
mahad <mahad@SAM>
2023-08-06 16:35:09 +0530

    Add project files.

---
+++ CustomerApp/Controllers/CustomerController.cs
@@ -0,0 +1,212 @@
+﻿using CustomerApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace CustomerApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public string errorMessage = "";
+        public string successMessage = "";
+
+        HttpClient client = new HttpClient();
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Customer> customers = new List<Customer>();
+            client.BaseAddress = new Uri("https://getinvoices.azurewebsites.net/api/Customers");
+            var response = client.GetAsync("Customers");
+            response.Wait();
+            var test = response.Result;
+
+            if (test.IsSuccessStatusCode)
+            {
+                string display = test.Content.ReadAsStringAsync().Result;
+                customers = JsonConvert.DeserializeObject<List<Customer>>(display);
+            }
+            return View(customers);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            try
+            {
+                client.BaseAddress = new Uri("https://getinvoices.azurewebsites.net/api/Customer");
+                //string data = JsonConvert.SerializeObject(customer);
+                //StringContent stringContent = new StringContent(data, Encoding.UTF8, "application/json");
+                var response = client.PostAsJsonAsync<Customer>("Customer", customer);
+                response.Wait();
+
+                var testresult = response.Result;
+                if (testresult.IsSuccessStatusCode)
+                {
+                    TempData["SuccessMessage"] = "Customer Added Successfuly !";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            Customer customers = new Customer();
+            try
+            {
+                client.BaseAddress = new Uri("https://getinvoices.azurewebsites.net/api/");
+                var response = client.GetAsync("Customer/" + Id);
+                response.Wait();
+                var test = response.Result;
+
+                if (test.IsSuccessStatusCode)
+                {
+                    string display = test.Content.ReadAsStringAsync().Result;
+                    customers = JsonConvert.DeserializeObject<Customer>(display);
+                }
+                return View(customers);
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+
+        }
+        [HttpPost]
+        public IActionResult Edit(Customer customer)
+        {
+            try
+            {
+                //string data = JsonConvert.SerializeObject(customer.id);
+                //StringContent stringContent = new StringContent(data, Encoding.UTF8, "application/json");
+                //client.BaseAddress = new Uri("https://getinvoices.azurewebsites.net/api/");
+                //var response = client.PutAsync("Customer/", stringContent);
+                client.BaseAddress = new Uri("https://getinvoices.azurewebsites.net/api/Customer");
+                //string data = JsonConvert.SerializeObject(customer);
+                //StringContent stringContent = new StringContent(data, Encoding.UTF8, "application/json");
+                var response = client.PutAsJsonAsync<Customer>("Customer/", customer);
+                response.Wait();
+
+                var testresult = response.Result;
+                if (testresult.IsSuccessStatusCode)
+                {
+                    TempData["SuccessMessage"] = "Customer Edited Successfuly !";
+                    return RedirectToAction("Index");
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+
+
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            try
+            {
+                Customer customers = new Customer();
+                client.BaseAddress = new Uri("https://getinvoices.azurewebsites.net/api/");
+                var response = client.GetAsync("Customers/" + Id);
+                response.Wait();
+                var test = response.Result;
+
+                if (test.IsSuccessStatusCode)
+                {
+                    string display = test.Content.ReadAsStringAsync().Result;
+                    customers = JsonConvert.DeserializeObject<Customer>(display);
+                }
+                return View(customers);
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeleteConfirm(int Id)
+        {
+            try
+            {
+                string data = JsonConvert.SerializeObject(Id);
+                StringContent stringContent = new StringContent(data, Encoding.UTF8, "application/json");
+                client.BaseAddress = new Uri("https://getinvoices.azurewebsites.net/api/");
+                var response = client.DeleteAsync("Customer/"+ Id);
+                response.Wait();
+
+                var testresult = response.Result;
+                if (testresult.IsSuccessStatusCode)
+                {
+                    TempData["SuccessMessage"] = "Customer Deleted !";
+                    return RedirectToAction("Index");
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GetCustomerList()
+        {
+            List<Customer> customers = new List<Customer>();
+            client.BaseAddress = new Uri("https://getinvoices.azurewebsites.net/api/CreateCustomerList");
+            var response = client.GetAsync("CreateCustomerList");
+            response.Wait();
+            var test = response.Result;
+
+            if (test.IsSuccessStatusCode)
+            {
+                string display = test.Content.ReadAsStringAsync().Result;
+                customers = JsonConvert.DeserializeObject<List<Customer>>(display);
+            }
+            return View(customers);
+        }
+
+        [HttpPost]
+        public IActionResult GetCustomerList(Customer customer)
+        {
+            try
+            {
+                client.BaseAddress = new Uri("https://getinvoices.azurewebsites.net/api/Customer");
+                string data = JsonConvert.SerializeObject(customer);
+                StringContent stringContent = new StringContent(data, Encoding.UTF8, "application/json");
+                var response = client.PostAsync("Customer", stringContent);
+                response.Wait();
+
+                var testresult = response.Result;
+                if (testresult.IsSuccessStatusCode)
+                {
+                    TempData["SuccessMessage"] = "Customer Added Successfuly !";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+            return View();
+        }
+    }
+}
---
+++ CustomerApp/Models/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomerApp.Models
+{
+    public class Customer
+    {
+        [DisplayName("Customer Id")]
+        public string? id { get; set; }
+        [DisplayName("First Name")]
+        public string? firstname { get; set; }
+        [DisplayName("Last Name")]
+        public string? lastname { get; set; }
+        [Required]
+        [DisplayName("Email")]
+        public string? email { get; set; }
+        [DisplayName("Phone Number")]
+        public string? phone_Number { get; set; }
+        [DisplayName("Country Code")]
+        public string? country_code { get; set; }
+        [DisplayName("Gender")]
+        public string? gender { get; set; }
+        [DisplayName("Balance")]
+        public decimal balance { get; set; }
+        public string? salutation { get; set; }
+        public string? initials { get; set; }
+        public string? firstname_ascii { get; set; }
+        public string? firstname_country_rank { get; set; }
+        public string? firstname_country_frequency { get; set; }
+        public string? lastname_ascii { get; set; }
+        public string? lastname_country_rank { get; set; }
+        public string? lastname_country_frequency { get; set; }
+        public string? password { get; set; }
+        public string? country_code_alpha { get; set; }
+        public string? country_name { get; set; }
+        public string? primary_language_code { get; set; }
+        public string? primary_language { get; set; }
+        public string? currency { get; set; }
+
+    }
+
+}
